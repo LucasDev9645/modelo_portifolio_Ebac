c8248117db70ced4aa54727e1fba293621a0a267
LucasDev <LucasDev9645@gmail.com>
2023-03-02 20:07:09 -0300

    iniciando estrutura portifolio EBAC

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/LucasDev9645.png" />
+    <Titulo>Lucas Freitas</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,15 @@
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre mim</Titulo>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nisi iste
+      minus nam atque praesentium temporibus perspiciatis inventore itaque
+      officia error, doloremque voluptatum voluptatem eveniet id libero
+      recusandae velit! Asperiores.
+    </p>
+  </section>
+)
+
+export default Sobre
