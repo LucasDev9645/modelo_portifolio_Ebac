84228446edd72e9a0bfd559a7a2426f78b4870f6
LucasDev <LucasDev9645@gmail.com>
2023-03-04 11:57:14 -0300

    estilizando sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/LucasDev9645.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/LucasDev9645.png" />
-    <Titulo fontSize={20}>Lucas Freitas</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Lucas Freitas</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        LucasDev
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
