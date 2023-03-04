e55d669f45b35f9db326708a3505013187d438ba
LucasDev <LucasDev9645@gmail.com>
2023-03-04 10:35:42 -0300

    seção "sobre mim"

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -3,7 +3,7 @@ import Titulo from '../../components/Titulo'
 const Sidebar = () => (
   <aside>
     <img src="https://github.com/LucasDev9645.png" />
-    <Titulo>Lucas Freitas</Titulo>
+    <Titulo fontSize={20}>Lucas Freitas</Titulo>
   </aside>
 )
 
--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,14 +1,20 @@
 import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
 
 const Sobre = () => (
   <section>
-    <Titulo>Sobre mim</Titulo>
-    <p>
+    <Titulo fontSize={16}>Sobre mim</Titulo>
+    <Paragrafo tipo="principal">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nisi iste
       minus nam atque praesentium temporibus perspiciatis inventore itaque
       officia error, doloremque voluptatum voluptatem eveniet id libero
       recusandae velit! Asperiores.
-    </p>
+    </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=LucasDev9645&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=LucasDev9645&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
   </section>
 )
 
